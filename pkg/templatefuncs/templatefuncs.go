dff8a5e4a3c6008b63bf7d12d1ee01c9ab65942b
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2022-12-30 15:44:46 +0100

    Added lots of functions, and missing tests

--- pkg/templatefuncs/templatefuncs.go
+++ pkg/templatefuncs/templatefuncs.go
@@ -20,13 +20,16 @@ package templatefuncs
 import (
 	"encoding/json"
 	"fmt"
+	"math"
 	"path/filepath"
+	"reflect"
 	"regexp"
 	"strconv"
 	"strings"
 	"text/template"
 
 	"github.com/RiskIdent/jelease/pkg/version"
+	"gopkg.in/typ.v4/slices"
 	"gopkg.in/yaml.v2"
 )
 
@@ -36,6 +39,7 @@ var (
 )
 
 var FuncsMap = template.FuncMap{
+	// Version functions
 	"versionBump": func(add string, to string) string {
 		// "1.2.3" | versionBump "0.1.0"
 		// add = 0.1.0
@@ -50,29 +54,8 @@ var FuncsMap = template.FuncMap{
 		}
 		return toVer.Bump(addVer).String()
 	},
-	"sanitizePath": func(path string) string {
-		path = strings.ToLower(path)
-		path = filepath.ToSlash(path)
-		return pathCharRegex.ReplaceAllLiteralString(path, "-")
-	},
-	"sanitizePathSegment": func(path string) string {
-		path = strings.ToLower(path)
-		return pathSegmentCharRegex.ReplaceAllLiteralString(path, "-")
-	},
-	"basename": func(path string) string {
-		return filepath.Base(path)
-	},
-	"dirname": func(path string) string {
-		return filepath.Dir(path)
-	},
-	"regexReplaceAll": func(regex, replace, text string) string {
-		re := regexp.MustCompile(regex)
-		return re.ReplaceAllString(text, replace)
-	},
-	"regexMatch": func(regex, text string) bool {
-		re := regexp.MustCompile(regex)
-		return re.MatchString(text)
-	},
+
+	// Type conversion functions
 	"int": func(value any) int {
 		switch value := value.(type) {
 		case string:
@@ -145,6 +128,73 @@ var FuncsMap = template.FuncMap{
 			panic(fmt.Sprintf("unsupported type: %T", value))
 		}
 	},
+
+	// List functions
+	"list": func(values ...any) []any {
+		return values
+	},
+	"first": func(list []any) any {
+		return list[0]
+	},
+	"rest": func(list []any) []any {
+		return list[1:]
+	},
+	"last": func(list []any) any {
+		return list[len(list)-1]
+	},
+	"initial": func(list []any) any {
+		return list[:len(list)-1]
+	},
+	"append": func(list []any, values ...any) []any {
+		return append(list, values...)
+	},
+	"prepend": func(list []any, values ...any) []any {
+		return append(values, list...)
+	},
+	"concat": func(a, b []any) []any {
+		return append(a, b...)
+	},
+	"reverse": func(list []any) []any {
+		reversed := make([]any, len(list))
+		copy(reversed, list)
+		slices.Reverse(reversed)
+		return reversed
+	},
+
+	// Dictionary functions
+	"dict": func(keyAndValues ...any) map[string]any {
+		if len(keyAndValues)%2 != 0 {
+			panic("must be an even number of arguments, for pairs of key + value")
+		}
+		dict := make(map[string]any)
+		for i := 0; i < len(keyAndValues); i += 2 {
+			key, keyIsString := keyAndValues[i].(string)
+			value := keyAndValues[i+1]
+
+			if !keyIsString {
+				panic(fmt.Sprintf("expected string for map key, got %T", keyAndValues[i]))
+			}
+			dict[key] = value
+		}
+		return dict
+	},
+	"get": func(dict map[string]any, key string) any {
+		return dict[key]
+	},
+	"set": func(dict map[string]any, key string, value any) map[string]any {
+		dict[key] = value
+		return dict
+	},
+	"unset": func(dict map[string]any, key string) map[string]any {
+		delete(dict, key)
+		return dict
+	},
+	"hasKey": func(dict map[string]any, key string) bool {
+		_, ok := dict[key]
+		return ok
+	},
+
+	// Encoding functions
 	"fromYaml": func(value string) any {
 		var result any
 		if err := yaml.Unmarshal([]byte(value), &result); err != nil {
@@ -180,6 +230,8 @@ var FuncsMap = template.FuncMap{
 		}
 		return result
 	},
+
+	// String functions
 	"trimPrefix": func(prefix, s string) string {
 		return strings.TrimPrefix(s, prefix)
 	},
@@ -203,5 +255,269 @@ var FuncsMap = template.FuncMap{
 	},
 	"hasSuffix": func(suffix, s string) bool {
 		return strings.HasSuffix(s, suffix)
+	},
+
+	// Path functions
+	"sanitizePath": func(path string) string {
+		path = strings.ToLower(path)
+		path = filepath.ToSlash(path)
+		return pathCharRegex.ReplaceAllLiteralString(path, "-")
+	},
+	"sanitizePathSegment": func(path string) string {
+		path = strings.ToLower(path)
+		return pathSegmentCharRegex.ReplaceAllLiteralString(path, "-")
+	},
+	"basename": func(path string) string {
+		return filepath.Base(path)
+	},
+	"dirname": func(path string) string {
+		return filepath.Dir(path)
+	},
+	"fileext": func(path string) string {
+		return filepath.Ext(path)
+	},
+
+	// Regex functions
+	"regexReplaceAll": func(regex, replace, text string) string {
+		re := regexp.MustCompile(regex)
+		return re.ReplaceAllString(text, replace)
+	},
+	"regexMatch": func(regex, text string) bool {
+		re := regexp.MustCompile(regex)
+		return re.MatchString(text)
+	},
+
+	// Math functions
+	"add": func(a, b any) any {
+		aType := reflect.TypeOf(a)
+		bType := reflect.TypeOf(b)
+		if aType != bType {
+			panic(fmt.Sprintf("args must be of same type, got %T and %T", a, b))
+		}
+		switch a := a.(type) {
+		case string:
+			return a + b.(string)
+		case int:
+			return a + b.(int)
+		case int8:
+			return a + b.(int8)
+		case int16:
+			return a + b.(int16)
+		case int32:
+			return a + b.(int32)
+		case int64:
+			return a + b.(int64)
+		case uint:
+			return a + b.(uint)
+		case uint8:
+			return a + b.(uint8)
+		case uint16:
+			return a + b.(uint16)
+		case uint32:
+			return a + b.(uint32)
+		case uint64:
+			return a + b.(uint64)
+		case float32:
+			return a + b.(float32)
+		case float64:
+			return a + b.(float64)
+		case complex64:
+			return a + b.(complex64)
+		case complex128:
+			return a + b.(complex128)
+		default:
+			panic(fmt.Sprintf("unsupported type: %T", a))
+		}
+	},
+	"sub": func(a, b any) any {
+		aType := reflect.TypeOf(a)
+		bType := reflect.TypeOf(b)
+		if aType != bType {
+			panic(fmt.Sprintf("args must be of same type, got %T and %T", a, b))
+		}
+		switch a := a.(type) {
+		case int:
+			return a - b.(int)
+		case int8:
+			return a - b.(int8)
+		case int16:
+			return a - b.(int16)
+		case int32:
+			return a - b.(int32)
+		case int64:
+			return a - b.(int64)
+		case uint:
+			return a - b.(uint)
+		case uint8:
+			return a - b.(uint8)
+		case uint16:
+			return a - b.(uint16)
+		case uint32:
+			return a - b.(uint32)
+		case uint64:
+			return a - b.(uint64)
+		case float32:
+			return a - b.(float32)
+		case float64:
+			return a - b.(float64)
+		case complex64:
+			return a - b.(complex64)
+		case complex128:
+			return a - b.(complex128)
+		default:
+			panic(fmt.Sprintf("unsupported type: %T", a))
+		}
+	},
+	"div": func(a, b any) any {
+		aType := reflect.TypeOf(a)
+		bType := reflect.TypeOf(b)
+		if aType != bType {
+			panic(fmt.Sprintf("args must be of same type, got %T and %T", a, b))
+		}
+		switch a := a.(type) {
+		case int:
+			return a / b.(int)
+		case int8:
+			return a / b.(int8)
+		case int16:
+			return a / b.(int16)
+		case int32:
+			return a / b.(int32)
+		case int64:
+			return a / b.(int64)
+		case uint:
+			return a / b.(uint)
+		case uint8:
+			return a / b.(uint8)
+		case uint16:
+			return a / b.(uint16)
+		case uint32:
+			return a / b.(uint32)
+		case uint64:
+			return a / b.(uint64)
+		case float32:
+			return a / b.(float32)
+		case float64:
+			return a / b.(float64)
+		case complex64:
+			return a / b.(complex64)
+		case complex128:
+			return a / b.(complex128)
+		default:
+			panic(fmt.Sprintf("unsupported type: %T", a))
+		}
+	},
+	"mod": func(a, b any) any {
+		aType := reflect.TypeOf(a)
+		bType := reflect.TypeOf(b)
+		if aType != bType {
+			panic(fmt.Sprintf("args must be of same type, got %T and %T", a, b))
+		}
+		switch a := a.(type) {
+		case int:
+			return a % b.(int)
+		case int8:
+			return a % b.(int8)
+		case int16:
+			return a % b.(int16)
+		case int32:
+			return a % b.(int32)
+		case int64:
+			return a % b.(int64)
+		case uint:
+			return a % b.(uint)
+		case uint8:
+			return a % b.(uint8)
+		case uint16:
+			return a % b.(uint16)
+		case uint32:
+			return a % b.(uint32)
+		case uint64:
+			return a % b.(uint64)
+		case float32:
+			return float32(math.Mod(float64(a), float64(b.(float32))))
+		case float64:
+			return math.Mod(a, b.(float64))
+		default:
+			panic(fmt.Sprintf("unsupported type: %T", a))
+		}
+	},
+	"mul": func(a, b any) any {
+		aType := reflect.TypeOf(a)
+		bType := reflect.TypeOf(b)
+		if aType != bType {
+			panic(fmt.Sprintf("args must be of same type, got %T and %T", a, b))
+		}
+		switch a := a.(type) {
+		case int:
+			return a * b.(int)
+		case int8:
+			return a * b.(int8)
+		case int16:
+			return a * b.(int16)
+		case int32:
+			return a * b.(int32)
+		case int64:
+			return a * b.(int64)
+		case uint:
+			return a * b.(uint)
+		case uint8:
+			return a * b.(uint8)
+		case uint16:
+			return a * b.(uint16)
+		case uint32:
+			return a * b.(uint32)
+		case uint64:
+			return a * b.(uint64)
+		case float32:
+			return a * b.(float32)
+		case float64:
+			return a * b.(float64)
+		case complex64:
+			return a * b.(complex64)
+		case complex128:
+			return a * b.(complex128)
+		default:
+			panic(fmt.Sprintf("unsupported type: %T", a))
+		}
+	},
+	"floor": func(a any) any {
+		switch value := a.(type) {
+		case int, int8, int16, int32, int64,
+			uint, uint8, uint16, uint32, uint64:
+			return value
+		case float32:
+			return float32(math.Floor(float64(value)))
+		case float64:
+			return math.Floor(value)
+		default:
+			panic(fmt.Sprintf("unsupported type: %T", value))
+		}
+	},
+	"ceil": func(a any) any {
+		switch value := a.(type) {
+		case int, int8, int16, int32, int64,
+			uint, uint8, uint16, uint32, uint64:
+			return value
+		case float32:
+			return float32(math.Ceil(float64(value)))
+		case float64:
+			return math.Ceil(value)
+		default:
+			panic(fmt.Sprintf("unsupported type: %T", value))
+		}
+	},
+	"round": func(a any) any {
+		switch value := a.(type) {
+		case int, int8, int16, int32, int64,
+			uint, uint8, uint16, uint32, uint64:
+			return value
+		case float32:
+			return float32(math.Round(float64(value)))
+		case float64:
+			return math.Round(value)
+		default:
+			panic(fmt.Sprintf("unsupported type: %T", value))
+		}
 	},
 }
--- pkg/templatefuncs/templatefuncs_test.go
+++ pkg/templatefuncs/templatefuncs_test.go
@@ -30,16 +30,19 @@ func TestTemplate(t *testing.T) {
 		want string
 		data any
 	}{
+		// Version functions
 		{
-			name: "regexReplaceAll",
-			tmpl: `{{"abbaba" | regexReplaceAll "b+" "."}}`,
-			want: "a.a.a",
+			name: "versionBump",
+			tmpl: `{{"1.2.3" | versionBump "0.1.0"}}`,
+			want: "1.3.0",
 		},
 		{
-			name: "regexMatch",
-			tmpl: `{{"abba" | regexMatch "b*"}}`,
-			want: "true",
+			name: "versionBump_2",
+			tmpl: `{{"v1.2.3-rc.123" | versionBump "v0.1.0"}}`,
+			want: "v1.3.0",
 		},
+
+		// Type conversion functions
 		{
 			name: "int_fromString",
 			tmpl: `{{int "36"}}`,
@@ -70,6 +73,97 @@ func TestTemplate(t *testing.T) {
 			tmpl: `{{float64 36.05}}`,
 			want: "36.05",
 		},
+
+		// List functions
+		{
+			name: "list",
+			tmpl: `{{list 1 true 3.5 "four"}}`,
+			want: `[1 true 3.5 four]`,
+		},
+		{
+			name: "first",
+			tmpl: `{{first .}}`,
+			want: `a`,
+			data: []any{"a", "b", "c", "d"},
+		},
+		{
+			name: "rest",
+			tmpl: `{{rest .}}`,
+			want: `[b c d]`,
+			data: []any{"a", "b", "c", "d"},
+		},
+		{
+			name: "last",
+			tmpl: `{{last .}}`,
+			want: `d`,
+			data: []any{"a", "b", "c", "d"},
+		},
+		{
+			name: "initial",
+			tmpl: `{{initial .}}`,
+			want: `[a b c]`,
+			data: []any{"a", "b", "c", "d"},
+		},
+		{
+			name: "append",
+			tmpl: `{{append . 1 2}}`,
+			want: `[a b 1 2]`,
+			data: []any{"a", "b"},
+		},
+		{
+			name: "prepend",
+			tmpl: `{{prepend . 1 2}}`,
+			want: `[1 2 a b]`,
+			data: []any{"a", "b"},
+		},
+		{
+			name: "concat",
+			tmpl: `{{concat .A .B}}`,
+			want: `[a b c d]`,
+			data: map[string]any{
+				"A": []any{"a", "b"},
+				"B": []any{"c", "d"},
+			},
+		},
+		{
+			name: "reverse",
+			tmpl: `{{reverse .}}`,
+			want: `[d c b a]`,
+			data: []any{"a", "b", "c", "d"},
+		},
+
+		// Dictionary functions
+		{
+			name: "dict",
+			tmpl: `{{dict "a" 1}}`,
+			want: `map[a:1]`,
+		},
+		{
+			name: "get",
+			tmpl: `{{get . "a"}}`,
+			want: `1`,
+			data: map[string]any{"a": 1},
+		},
+		{
+			name: "set",
+			tmpl: `{{set . "a" 5}}`,
+			want: `map[a:5]`,
+			data: map[string]any{"a": 1},
+		},
+		{
+			name: "unset",
+			tmpl: `{{unset . "a"}}`,
+			want: `map[]`,
+			data: map[string]any{"a": 1},
+		},
+		{
+			name: "hasKey",
+			tmpl: `{{hasKey . "a"}}`,
+			want: `true`,
+			data: map[string]any{"a": 1},
+		},
+
+		// Encoding functions
 		{
 			name: "toPrettyJson",
 			tmpl: `{{toPrettyJson .}}`,
@@ -107,6 +201,164 @@ func TestTemplate(t *testing.T) {
 					"Name": "Bangladesh",
 				},
 			},
+		},
+
+		// String functions
+		{
+			name: "trimPrefix",
+			tmpl: `{{"www.example.cc" | trimPrefix "w"}}`,
+			want: "ww.example.cc",
+		},
+		{
+			name: "trimSuffix",
+			tmpl: `{{"www.example.cc" | trimSuffix "c"}}`,
+			want: "www.example.c",
+		},
+		{
+			name: "trimSpace",
+			tmpl: `{{"  \t www.example.cc \n" | trimSpace}}`,
+			want: "www.example.cc",
+		},
+		{
+			name: "trim",
+			tmpl: `{{"www.example.cc" | trim "wc."}}`,
+			want: "example",
+		},
+		{
+			name: "trimRight",
+			tmpl: `{{"www.example.cc" | trimRight "wc."}}`,
+			want: "www.example",
+		},
+		{
+			name: "trimLeft",
+			tmpl: `{{"www.example.cc" | trimLeft "wc."}}`,
+			want: "example.cc",
+		},
+		{
+			name: "hasPrefix",
+			tmpl: `{{"www.example.cc" | hasPrefix "www."}}`,
+			want: "true",
+		},
+		{
+			name: "hasSuffix",
+			tmpl: `{{"www.example.cc" | hasSuffix ".cc"}}`,
+			want: "true",
+		},
+
+		// Path functions
+		{
+			name: "sanitizePath",
+			tmpl: `{{sanitizePath "/some wiärd päth/to/some/place"}}`,
+			want: "/some-wi-rd-p-th/to/some/place",
+		},
+		{
+			name: "sanitizePathSegment",
+			tmpl: `{{sanitizePathSegment "/some wiärd päth/to/some/place"}}`,
+			want: "-some-wi-rd-p-th-to-some-place",
+		},
+		{
+			name: "basename",
+			tmpl: `{{basename "/some/long/path/to/a/file.txt"}}`,
+			want: "file.txt",
+		},
+		{
+			name: "dirname",
+			tmpl: `{{dirname "/some/long/path/to/a/file.txt"}}`,
+			want: "/some/long/path/to/a",
+		},
+		{
+			name: "fileext",
+			tmpl: `{{fileext "/some/long/path/to/a/file.txt"}}`,
+			want: ".txt",
+		},
+
+		// Regex functions
+		{
+			name: "regexReplaceAll",
+			tmpl: `{{"abbaba" | regexReplaceAll "b+" "."}}`,
+			want: "a.a.a",
+		},
+		{
+			name: "regexMatch",
+			tmpl: `{{"abba" | regexMatch "b*"}}`,
+			want: "true",
+		},
+
+		// Math functions
+		{
+			name: "add_int",
+			tmpl: `{{add 1 2}}`,
+			want: `3`,
+		},
+		{
+			name: "add_float",
+			tmpl: `{{add 1.5 2.1}}`,
+			want: `3.6`,
+		},
+		{
+			name: "add_string",
+			tmpl: `{{add "ab" "ba"}}`,
+			want: `abba`,
+		},
+		{
+			name: "sub_int",
+			tmpl: `{{sub 1 2}}`,
+			want: `-1`,
+		},
+		{
+			name: "sub_float",
+			tmpl: `{{sub 1.5 2.1 | printf "%.1f"}}`,
+			want: `-0.6`,
+		},
+		{
+			name: "div_int",
+			tmpl: `{{div 10 2}}`,
+			want: `5`,
+		},
+		{
+			name: "div_float",
+			tmpl: `{{div 4.5 3.0 | printf "%.1f"}}`,
+			want: `1.5`,
+		},
+		{
+			name: "mod_int",
+			tmpl: `{{mod 10 2}}`,
+			want: `0`,
+		},
+		{
+			name: "mod_float",
+			tmpl: `{{mod 1.1 1.0 | printf "%.1f"}}`,
+			want: `0.1`,
+		},
+		{
+			name: "mul_int",
+			tmpl: `{{mul 10 2}}`,
+			want: `20`,
+		},
+		{
+			name: "mul_float",
+			tmpl: `{{mul 1.1 10.0 | printf "%.1f"}}`,
+			want: `11.0`,
+		},
+		{
+			name: "floor",
+			tmpl: `{{floor 10.9}}`,
+			want: `10`,
+		},
+		{
+			name: "ceil",
+			tmpl: `{{ceil 10.9}}`,
+			want: `11`,
+		},
+		{
+			name: "round_up",
+			tmpl: `{{round 10.9}}`,
+			want: `11`,
+		},
+		{
+			name: "round_down",
+			tmpl: `{{round 10.2}}`,
+			want: `10`,
 		},
 	}
 
